8ae0ff1ba832223e6f9bc19278ed5b1b7c2ee458
desplora <desploraviajes@gmail.com>
2024-03-26 20:15:56 +0100

    ovejo aprendiendo

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,4 @@
-import { sql } from '@vercel/postgres';
+import { sql } from '@vercel/postgres'; // la funcion libreria de postgres
 import {
   CustomerField,
   CustomersTableType,
@@ -18,8 +18,8 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    //console.log('Fetching revenue data...');
+    //await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
