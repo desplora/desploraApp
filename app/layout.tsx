9b10e97b28bc2d2df651938898780bab903fd0c1
desplora <desploraviajes@gmail.com>
2024-03-26 15:46:33 +0100

    predeploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
